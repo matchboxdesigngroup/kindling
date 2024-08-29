bdfb74806b22b6ba5e18d21e9575977e4924ffa0
Steve Struemph <sstruemph@matchboxdesigngroup.com>
2024-08-29 14:30:31 -0500

    refactor(mobile-site-logo): Format for options conditional and add i18n

--- assets/block-extensions/mobile-site-logo/index.js
+++ assets/block-extensions/mobile-site-logo/index.js
@@ -4,50 +4,50 @@ import { MediaUpload } from '@wordpress/block-editor';
 import { Button } from '@wordpress/components';
 import { registerBlockType } from '@wordpress/blocks';
 
-addFilter(
-  'blocks.registerBlockType',
-  'kindling/add-second-site-logo',
-  (settings, name) => {
-    if (name !== 'core/site-logo') {
-      return settings;
-    }
 // Check if the extension should be enabled
 if (typeof mobileSiteLogoEnabled !== 'undefined' && mobileSiteLogoEnabled) {
+  addFilter(
+    'blocks.registerBlockType',
+    'kindling/mobile-site-logo',
+    (settings, name) => {
+      if (name !== 'core/site-logo') {
+        return settings;
+      }
 
-    const { attributes, edit } = settings;
+      const { attributes, edit } = settings;
 
-    attributes.logo2 = {
-      type: 'integer',
-    };
-
-    settings.edit = (props) => {
-      const { setAttributes } = props;
-      const logo2 = props.attributes.logo2;
-
-      const onSelectLogo2 = (media) => {
-        setAttributes({ logo2: media.id });
+      attributes.logo2 = {
+        type: 'integer',
       };
 
-      return (
-        <div>
-          {edit(props)}
-          <div>
-            <MediaUpload
-              onSelect={onSelectLogo2}
-              allowedTypes={['image']}
-              value={logo2}
-              render={({ open }) => (
-                <Button onClick={open}>
-                  Select Logo For Mobile
-                </Button>
-              )}
-            />
-          </div>
-        </div>
-      );
-    };
+      settings.edit = (props) => {
+        const { setAttributes } = props;
+        const logo2 = props.attributes.logo2;
 
-    return settings;
-  }
-);
+        const onSelectLogo2 = (media) => {
+          setAttributes({ logo2: media.id });
+        };
+
+        return (
+          <div>
+            {edit(props)}
+            <div>
+              <MediaUpload
+                onSelect={onSelectLogo2}
+                allowedTypes={['image']}
+                value={logo2}
+                render={({ open }) => (
+                  <Button onClick={open}>
+                    {__('Select Logo For Mobile', 'kindling')}
+                  </Button>
+                )}
+              />
+            </div>
+          </div>
+        );
+      };
+
+      return settings;
+    }
+  );
 };
