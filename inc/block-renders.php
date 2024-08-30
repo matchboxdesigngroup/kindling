9d8b6fdaa38b82399c55ed3ad2b9508b03ad2e17
Steve Struemph <sstruemph@matchboxdesigngroup.com>
2024-08-30 12:05:44 -0500

    cleanup: Remove child page navigation block

--- inc/block-renders.php
+++ inc/block-renders.php
@@ -1,19 +1,15 @@
 <?php
 
 /* Custom block server-side renders */
-include dirname(__DIR__) . '/assets/blocks/child-page-navigation-link/index.php';
+// include dirname(__DIR__) . '/assets/block-extensions/block-name/index.php';
 
 /* Custom block register when using block.json */
-add_action( 'init', 'register_block_child_page_navigation_link' );
+// add_action('init', 'register_block_name');
 
 /* Block extension server-side renders */
 include dirname(__DIR__) . '/assets/block-extensions/block-toolbar/block-links/index.php';
 include dirname(__DIR__) . '/assets/block-extensions/mobile-site-logo/index.php';
 include dirname(__DIR__) . '/assets/block-extensions/safe-svg/index.php';
-
-
-
-
 
 /**
  * Query Pagination Previous & Next - Extension
@@ -22,32 +18,32 @@ include dirname(__DIR__) . '/assets/block-extensions/safe-svg/index.php';
  *
  * Adds a toggle to remove the link text while keeping the <a></a> element
  */
-add_filter( 'render_block', function( $block_content, $block ) {
-    if ( 'core/query-pagination-previous' === $block['blockName'] ) {
-        $disable_text = true;
+add_filter('render_block', function ($block_content, $block) {
+  if ('core/query-pagination-previous' === $block['blockName']) {
+    $disable_text = true;
 
-        if ( $disable_text ) {
-            preg_match( '/<a href="([^"]*)"[^>]*>/', $block_content, $matches );
-            $href = $matches[1] ?? '#';
+    if ($disable_text) {
+      preg_match('/<a href="([^"]*)"[^>]*>/', $block_content, $matches);
+      $href = $matches[1] ?? '#';
 
-            $block_content = preg_replace( '/<a[^>]*>(.*?)<\/a>/', "<a class=\"wp-block-query-pagination-previous\" href=\"{$href}\" aria-label=\"Previous Page\"> </a>", $block_content );
-        }
+      $block_content = preg_replace('/<a[^>]*>(.*?)<\/a>/', "<a class=\"wp-block-query-pagination-previous\" href=\"{$href}\" aria-label=\"Previous Page\"> </a>", $block_content);
     }
+  }
 
-    return $block_content;
-}, 10, 2 );
+  return $block_content;
+}, 10, 2);
 
-add_filter( 'render_block', function( $block_content, $block ) {
-    if ( 'core/query-pagination-next' === $block['blockName'] ) {
-        $disable_text = true;
+add_filter('render_block', function ($block_content, $block) {
+  if ('core/query-pagination-next' === $block['blockName']) {
+    $disable_text = true;
 
-        if ( $disable_text ) {
-            preg_match( '/<a href="([^"]*)"[^>]*>/', $block_content, $matches );
-            $href = $matches[1] ?? '#';
+    if ($disable_text) {
+      preg_match('/<a href="([^"]*)"[^>]*>/', $block_content, $matches);
+      $href = $matches[1] ?? '#';
 
-            $block_content = preg_replace( '/<a[^>]*>(.*?)<\/a>/', "<a class=\"wp-block-query-pagination-next\" href=\"{$href}\" aria-label=\"Previous Page\"> </a>", $block_content );
-        }
+      $block_content = preg_replace('/<a[^>]*>(.*?)<\/a>/', "<a class=\"wp-block-query-pagination-next\" href=\"{$href}\" aria-label=\"Previous Page\"> </a>", $block_content);
     }
+  }
 
-    return $block_content;
-}, 10, 2 );
+  return $block_content;
+}, 10, 2);
